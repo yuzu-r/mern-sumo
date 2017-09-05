e94e6d2b1db12909449882e8722ef3a5f3db87ff
yuzu-r <tkaneshiro@gmail.com>
2017-09-05 23:20:25 +0000

    mongoose connection

--- server/server.js
+++ server/server.js
@@ -1,16 +1,21 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import path from 'path';
+import mongoose from 'mongoose';
+import Location from './model/locations';
 
 const app = express();
 const router = express.Router();
+
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/learn-mern';
+mongoose.connect(mongoURI);
 
 // get reference to the client build directory
 const staticFiles = express.static(path.join(__dirname, '../../client/build'));
 
 // pass the static files (react app) to the express app. 
 app.use(staticFiles);
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
@@ -27,6 +32,25 @@ router.get('/cities',(req,res) => {
 	];
 	res.json(cities);
 });
+
+router.route('/locations')
+	.get(function(req,res) {
+		Location.find(function(err, locations) {
+			if (err)
+				res.send(err);
+			res.json(locations);
+		});
+	})
+	.post(function(req, res) {
+		var location = new Location();
+		location.city = req.body.city;
+		location.info = req.body.info;
+		location.save(function(err) {
+			if (err)
+				res.send(err);
+			res.json({message: 'location successfully added!'});
+		});
+	});
 
 app.use(router);
 app.use('/*', staticFiles);
